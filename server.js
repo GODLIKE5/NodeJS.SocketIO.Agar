376e738c2458241a4cf5004da4c79deebec54e2d
DESKTOP-U223FLP\BURAK <burakgumusfb@gmail.com>
2016-10-15 19:21:51 +0100

    three js important.

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
+app.use(express.static(__dirname+'/public'));
+
 io.on('connection', function(socket){
     console.log('a user connected');
 });
