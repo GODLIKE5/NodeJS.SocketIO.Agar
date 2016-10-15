a38c809a86f2ff6feac51c73ca2e5f9ebbe7e5c7
DESKTOP-U223FLP\BURAK <burakgumusfb@gmail.com>
2016-10-15 23:33:33 +0100

    no message

--- server.js
+++ server.js
@@ -5,10 +5,28 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var path = require('path');
 
 app.use(express.static(__dirname+'/public'));
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:31');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 io.on('connection', function(socket){
     console.log('a user connected');
 });
